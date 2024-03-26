17935795b89751f1a1f3a2a3bccc4087ad000f70
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2024-03-26 21:01:37 +0545

    Week 2 updates

---
+++ Computing3/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Computing3.Data;
+using Computing3.Models.Student;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Computing3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+
+        public StudentController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStudent(Student request)
+        {
+            var AddStudent = new Student()
+            {
+                Name = request.Name,
+                Hobby = request.Hobby
+            };
+
+            await dbContext.students.AddAsync(AddStudent);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(AddStudent);
+        }
+    }
+}
---
+++ Computing3/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Computing3.Models.Student;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Computing3.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> students {  get; set; }       
+    }
+}
