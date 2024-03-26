a14dcb942e4a7371929ae11a92dae2a3ed42a0ec
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2024-03-26 20:58:01 +0545

    Week 1 updates

---
+++ Computing3/Models/Student/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Computing3.Models.Student
+{
+    public class Student
+    {
+        [Key]
+        public int LMUID { get; set; }
+        public string Name { get; set; }
+        public string Hobby { get; set; }
+
+    }
+}
